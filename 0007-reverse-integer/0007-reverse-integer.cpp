901bbfa5d0c9b2a95a9f10a95975d03c6284419b
Mohammed Sofiyan Pasha <spm2592007@gmail.com>
2026-08-21 21:16:36 +0530

    Time: 0 ms (100%), Space: 8.5 MB (83.71%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int reversed=0;
+
+        while(x!=0){
+            int digit=x%10;
+            x/=10;
+         if (reversed > INT_MAX / 10 || reversed < INT_MIN / 10) return 0;
+                reversed=reversed*10+digit;
+        }
+        return reversed;
+        
+    }
+};
